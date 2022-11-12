4272c31c6c54ca459fe051b7106b777e86e69c17
SalmanAd01 <salmanadhikari415@gmail.com>
2022-11-12 22:53:15 +0530

    fix typo

--- src/db/connect.rs
+++ src/db/connect.rs
@@ -1,7 +1,7 @@
 use mongodb::{options::ClientOptions, Client, Database};
 
 pub async fn connect() -> Database {
-    let client_options = ClientOptions::parse(std::env::var("DB_URI").expect("MAILCOACH_API_TOKEN must be set.")).await.unwrap();
+    let client_options = ClientOptions::parse(std::env::var("DB_URI").expect("DB_URI must be set.")).await.unwrap();
     let client = Client::with_options(client_options).unwrap();
     let db = client.database("arenabackup");
     db
